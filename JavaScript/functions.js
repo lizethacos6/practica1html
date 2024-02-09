d49093d85e4eb8ac165436a5d5f1883bc1604ac6
lizethacos6 <acostaenriquezstephanielizeth@gmail.com>
2024-02-09 12:08:41 -0600

    Se agrego mensaje y tipo formulario

---
+++ JavaScript/functions.js
@@ -0,0 +1,8 @@
+function changeColor(){ 
+document.getElementById("palabrita").style.backgroundColor="green";
+document.getElementById("palabrita").textContent="hola";
+}
+
+function enviarMsg(){
+ alert("hola amigo");
+} 
